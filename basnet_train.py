4cbabd94d59a7c4b564e47eb11b7cca0eac5616d
hanat <hanat.aidai@aihuishou.com>
2021-06-17 14:37:34 +0800

    training

--- basnet_train.py
+++ basnet_train.py
@@ -1,3 +1,5 @@
+import os
+
 import torch
 import torchvision
 from torch.autograd import Variable
@@ -28,54 +30,55 @@ import pytorch_iou
 # ------- 1. define loss function --------
 
 bce_loss = nn.BCELoss(size_average=True)
-ssim_loss = pytorch_ssim.SSIM(window_size=11,size_average=True)
+ssim_loss = pytorch_ssim.SSIM(window_size=11, size_average=True)
 iou_loss = pytorch_iou.IOU(size_average=True)
 
-def bce_ssim_loss(pred,target):
 
-	bce_out = bce_loss(pred,target)
-	ssim_out = 1 - ssim_loss(pred,target)
-	iou_out = iou_loss(pred,target)
+def bce_ssim_loss(pred, target):
+    bce_out = bce_loss(pred, target)
+    ssim_out = 1 - ssim_loss(pred, target)
+    iou_out = iou_loss(pred, target)
 
-	loss = bce_out + ssim_out + iou_out
+    loss = bce_out + ssim_out + iou_out
 
-	return loss
+    return loss
+
 
 def muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, d7, labels_v):
+    loss0 = bce_ssim_loss(d0, labels_v)
+    loss1 = bce_ssim_loss(d1, labels_v)
+    loss2 = bce_ssim_loss(d2, labels_v)
+    loss3 = bce_ssim_loss(d3, labels_v)
+    loss4 = bce_ssim_loss(d4, labels_v)
+    loss5 = bce_ssim_loss(d5, labels_v)
+    loss6 = bce_ssim_loss(d6, labels_v)
+    loss7 = bce_ssim_loss(d7, labels_v)
+    # ssim0 = 1 - ssim_loss(d0,labels_v)
 
-	loss0 = bce_ssim_loss(d0,labels_v)
-	loss1 = bce_ssim_loss(d1,labels_v)
-	loss2 = bce_ssim_loss(d2,labels_v)
-	loss3 = bce_ssim_loss(d3,labels_v)
-	loss4 = bce_ssim_loss(d4,labels_v)
-	loss5 = bce_ssim_loss(d5,labels_v)
-	loss6 = bce_ssim_loss(d6,labels_v)
-	loss7 = bce_ssim_loss(d7,labels_v)
-	#ssim0 = 1 - ssim_loss(d0,labels_v)
+    # iou0 = iou_loss(d0,labels_v)
+    # loss = torch.pow(torch.mean(torch.abs(labels_v-d0)),2)*(5.0*loss0 + loss1 + loss2 + loss3 + loss4 + loss5) #+ 5.0*lossa
+    loss = loss0 + loss1 + loss2 + loss3 + loss4 + loss5 + loss6 + loss7  # + 5.0*lossa
+    print("l0: %3f, l1: %3f, l2: %3f, l3: %3f, l4: %3f, l5: %3f, l6: %3f\n" % (
+    loss0.data[0], loss1.data[0], loss2.data[0], loss3.data[0], loss4.data[0], loss5.data[0], loss6.data[0]))
+    # print("BCE: l1:%3f, l2:%3f, l3:%3f, l4:%3f, l5:%3f, la:%3f, all:%3f\n"%(loss1.data[0],loss2.data[0],loss3.data[0],loss4.data[0],loss5.data[0],lossa.data[0],loss.data[0]))
 
-	# iou0 = iou_loss(d0,labels_v)
-	#loss = torch.pow(torch.mean(torch.abs(labels_v-d0)),2)*(5.0*loss0 + loss1 + loss2 + loss3 + loss4 + loss5) #+ 5.0*lossa
-	loss = loss0 + loss1 + loss2 + loss3 + loss4 + loss5 + loss6 + loss7#+ 5.0*lossa
-	print("l0: %3f, l1: %3f, l2: %3f, l3: %3f, l4: %3f, l5: %3f, l6: %3f\n"%(loss0.data[0],loss1.data[0],loss2.data[0],loss3.data[0],loss4.data[0],loss5.data[0],loss6.data[0]))
-	# print("BCE: l1:%3f, l2:%3f, l3:%3f, l4:%3f, l5:%3f, la:%3f, all:%3f\n"%(loss1.data[0],loss2.data[0],loss3.data[0],loss4.data[0],loss5.data[0],lossa.data[0],loss.data[0]))
-
-	return loss0, loss
+    return loss0, loss
 
 
 # ------- 2. set the directory of training dataset --------
 
 data_dir = './train_data/'
-tra_image_dir = 'DUTS/DUTS-TR/DUTS-TR/im_aug/'
-tra_label_dir = 'DUTS/DUTS-TR/DUTS-TR/gt_aug/'
+tra_image_dir = './dataset/images/training/'
+tra_label_dir = './dataset/annotations/training/'
 
 image_ext = '.jpg'
 label_ext = '.png'
 
 model_dir = "./saved_models/basnet_bsi/"
-
+os.makedirs(model_dir, exist_ok=True)
 
 epoch_num = 100000
-batch_size_train = 8 
+batch_size_train = 8
 batch_size_val = 1
 train_num = 0
 val_num = 0
@@ -84,15 +87,15 @@ tra_img_name_list = glob.glob(data_dir + tra_image_dir + '*' + image_ext)
 
 tra_lbl_name_list = []
 for img_path in tra_img_name_list:
-	img_name = img_path.split("/")[-1]
+    img_name = img_path.split("/")[-1]
 
-	aaa = img_name.split(".")
-	bbb = aaa[0:-1]
-	imidx = bbb[0]
-	for i in range(1,len(bbb)):
-		imidx = imidx + "." + bbb[i]
+    aaa = img_name.split(".")
+    bbb = aaa[0:-1]
+    imidx = bbb[0]
+    for i in range(1, len(bbb)):
+        imidx = imidx + "." + bbb[i]
 
-	tra_lbl_name_list.append(data_dir + tra_label_dir + imidx + label_ext)
+    tra_lbl_name_list.append(data_dir + tra_label_dir + imidx + label_ext)
 
 print("---")
 print("train images: ", len(tra_img_name_list))
@@ -164,11 +167,13 @@ for epoch in range(0, epoch_num):
         del d0, d1, d2, d3, d4, d5, d6, d7, loss2, loss
 
         print("[epoch: %3d/%3d, batch: %5d/%5d, ite: %d] train loss: %3f, tar: %3f " % (
-        epoch + 1, epoch_num, (i + 1) * batch_size_train, train_num, ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
+            epoch + 1, epoch_num, (i + 1) * batch_size_train, train_num, ite_num, running_loss / ite_num4val,
+            running_tar_loss / ite_num4val))
 
         if ite_num % 2000 == 0:  # save model every 2000 iterations
 
-            torch.save(net.state_dict(), model_dir + "basnet_bsi_itr_%d_train_%3f_tar_%3f.pth" % (ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
+            torch.save(net.state_dict(), model_dir + "basnet_bsi_itr_%d_train_%3f_tar_%3f.pth" % (
+            ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
             running_loss = 0.0
             running_tar_loss = 0.0
             net.train()  # resume train
